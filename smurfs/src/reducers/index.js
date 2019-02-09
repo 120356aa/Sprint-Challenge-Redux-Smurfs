37eae58b22e958c7320137938e906ead76e63af5
120356aa <yurlovandrew@gmail.com>
2019-02-09 10:10:33 -0800

    refractored reducers code

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,45 @@
-import { combineReducers } from 'redux';
-import { smurfsReducer } from './smurfsreducers';
-
-export default combineReducers ({
-    smurfsReducer
-})
+import {
+    FETCH,
+    SUCCESS,
+    ERROR,
+    ADD_SMURF,
+    ADD_SUCCESS,
+    ADD_ERROR
+  } from '../actions';
+  
+  const initialState = {
+    smurfs: [],
+    error: null,
+    fetching: false
+  }
+  
+  const smurfReducer = (state = initialState, action) => {
+    switch(action.type) {
+      case ADD_SMURF:
+        return { ...state };
+      case ADD_SUCCESS:
+        return{
+          ...state,
+          error: null,
+          smurfs: action.payload
+        };
+      case ADD_ERROR:
+        return{ ...state, error: action.payload };
+      case FETCH:
+        return { ...state, fetching: true};
+      case SUCCESS:
+        return{
+          ...state,
+          error: null,
+          smurfs: action.payload,
+          fetching: false
+        };
+      case ERROR:
+        return{ ...state, error: action.payload, fetching: false};
+      default:
+        return state;
+    }
+  }
+  
+  export default smurfReducer;
+  
