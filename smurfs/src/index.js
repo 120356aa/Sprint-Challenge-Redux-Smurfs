60c8e147893dd0d3c953541840be321ccf57b1ee
120356aa <yurlovandrew@gmail.com>
2019-02-09 10:09:53 -0800

    adjusted index for less complexity

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import App from './App';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import rootReducer from "./reducers";
+import smurfReducer from './reducers';
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+const store = createStore(smurfReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
