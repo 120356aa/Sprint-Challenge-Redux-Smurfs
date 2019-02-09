a63cc60d79ff65eae67f8b4fb0e521d8721d4206
120356aa <yurlovandrew@gmail.com>
2019-02-09 10:11:14 -0800

    refractored actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,39 +1,35 @@
 import axios from 'axios';
- 
-export const FETCH_SMURFS = 'FETCH_SMURFS'
-export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS'
-export const FETCH_SMURFS_FAILURE = 'FETCH_SMURFS_FAILURE'
 
-export const ADD_SMURFS = 'ADD_SMURFS'
-export const ADD_SMURFS_SUCCESS = 'ADD_SMURFS_SUCCESS'
-export const ADD_SMURFS_FAILURE = 'ADD_SMURFS_FAILURE'
+export const FETCH = 'FETCH';
+export const SUCCESS = 'SUCCESS';
+export const ERROR = 'ERROR';
 
-const url = `http://localhost:3333/smurfs`
+export const ADD_SMURF = 'ADD_SMURF';
+export const ADD_SUCCESS = 'ADD_SUCCESS';
+export const ADD_ERROR = 'ADD_ERROR';
 
-export const fetchSmurfs = () => {
-  return dispatch => {
-    dispatch({ type: FETCH_SMURFS });
-
-    axios
-      .get(url)
-      .then(res => {
-        dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data })
-      })
-      .catch(err => {
-        dispatch({ type: FETCH_SMURFS_FAILURE, payload: err })
-      });
-  };
-}
-
-export const addSmurfs = smurf => dispatch => {
-  dispatch({ type: ADD_SMURFS });
-
+export const fetchSmurf = () => (dispatch) => {
+  dispatch({ type: FETCH });
   axios
-    .post(url, smurf)
-    .then(res => {
-      dispatch({ type: ADD_SMURFS_SUCCESS, payload: res.data })
+    .get(`http://localhost:3333/smurfs`)
+    .then(response => {
+      dispatch({ type: SUCCESS, payload: response.data})
     })
     .catch(err => {
-      dispatch({ type: ADD_SMURFS_FAILURE, payload: err})
+      dispatch({ type: ERROR, payload: err});
     });
-}
+};
+
+export const addSmurf = smurf => {
+  const newSmurf = axios.post('http://localhost:3333/smurfs', smurf);
+  return dispatch => {
+    dispatch({ type: ADD_SMURF });
+    newSmurf
+      .then(({ data }) => {
+        dispatch({ type: ADD_SUCCESS, payload: data });
+      })
+      .catch(err => {
+        dispatch({ type: ADD_ERROR, payload: err });
+      });
+  };
+};
